36dd7ba729ec910f9efc5978cd736b05df9d4762
allexcho <allexcho98@gmail.com>
2024-09-02 22:57:51 -0300

    clase 1

---
+++ Fundamentos de Javascript/main.js
@@ -0,0 +1,57 @@
+// Javascript es un lenguaje no copilado.
+
+// Frontend -
+// -CSS -HTML -JS -TypeScript (Frameworks -React -Angular -Vue.JS) (Frameworks sirve para optimizar la pagina)
+
+// Backend - 
+// -Servidor(-Node.JS -PHP -C -Python -C Sharp) -Base de datos(-No relaciones -MariaDB -MongoDB)-(Relaciones -SQL -MySQL -SQLite)
+
+
+// Elementos de JavaScript
+// -Datos -Variables -Objetos -Arrays -Funciones
+
+// Datos
+// -Number -String -Booleans -Console Log
+
+// Variables
+// -Global -Local
+
+// Objects
+// -Arrays -Booleans -Date -Math -Strings 
+
+// Arrays
+// -Index -Length
+
+// Functions 
+// -Arguments
+
+
+// Palabras Reservadas: (JAVASCRIPT)
+// -var -let -const  
+
+// Otras propiedades de otros lenguajes:
+// -CASE -THIS -IF -WHILE -WITH -NEW -COID -FUNCTION -TRY -RETURN -DEFAULT -ETC
+
+
+// Declaracion - Asignacion - Valor 
+
+// Declaracion          
+// const               
+
+// Asignacion
+// const name
+
+// Valor
+// const name = "Alex"
+
+
+// Sentencias -Prompt -Alert -Console log
+
+// Prompt
+// let name = prompt("Please insert your name:") (Prompt is basiclly an alert that can be modified)
+
+// Alert 
+// let name = alert("Congratulations! Your logged in!") (A popup that tells you you're logged in)
+
+// Console log
+// let name = name; console.log("name") (Shows results in your console)
