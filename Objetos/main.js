2da78afea2d5c8929ea3dfad204b38b931adef8e
Alex cho <allexcho98@gmail.com>
2024-10-27 19:30:15 -0300

    Mas ejercicios hechos

--- Objetos/main.js
+++ Objetos/main.js
@@ -220,46 +220,100 @@ Escriba un programa en JavaScript que cree una clase llamada "BankAccount" con p
 
 // Function class 1:
 
-class Product {
-  constructor(sku, description, price) {
-    this.sku = sku;
-    this.description = description;
-    this.price = price;
+// class Product {
+//   constructor(sku, description, price) {
+//     this.sku = sku;
+//     this.description = description;
+//     this.price = price;
+//   }
+//   articulo() {
+//     if (this.sku) {
+//       console.log(
+//         `Articulo: ${this.sku}. \nDescription: ${this.description}. \nPrecio: ${this.price}.`
+//       );
+//     } else {
+//       console.log(`No se encontro ningun articulo: ${this.sku}.`);
+//     }
+//   }
+// }
+
+// const producto = new Product("CA1", "Camisa rayada", 19900);
+// producto.articulo();
+
+// // Function class 2:
+
+// class Auto {
+//   constructor(marca, modelo, km) {
+//     this.marca = marca;
+//     this.modelo = modelo;
+//     this.km = km;
+//   }
+//   marcaAuto() {
+//     if (this.marca && this.modelo) {
+//       console.log(
+//         `Marca: ${this.marca}. \nModelo: ${this.modelo}. \nKM: ${this.km}.`
+//       );
+//     } else {
+//       console.log(
+//         `No se encontro la marca ${this.marca} ni modelo ${this.modelo}`
+//       );
+//     }
+//   }
+// }
+
+// const autito = new Auto("Toyota", "Corolla", 60000);
+// autito.marcaAuto();
+
+// Function class 3:
+
+// class Drinks {
+//   constructor(name, size, price) {
+//     this.name = name;
+//     this.size = size;
+//     this.price = price;
+//   }
+
+//   product() {
+//     if (this.name && this.size && this.price) {
+//       console.log(
+//         `Product: ${this.name} \nSize: ${
+//           this.size
+//         } \nPrice: $${this.price.toFixed(2)}`
+//       );
+//     } else {
+//       console.log("Please select a product...");
+//     }
+//   }
+
+//   discount(descuento) {
+//     if (descuento > 0 && descuento <= 100) {
+//       const discountedPrice = this.price * (1 - descuento / 100);
+//       this.price = discountedPrice > 0 ? discountedPrice : 0;
+//     } else {
+//       console.log("Invalid discount. Please enter a value between 1 and 100.");
+//     }
+//   }
+// }
+
+// const newDrink = new Drinks("Cola", "Tall", 10);
+// newDrink.discount(5);
+// newDrink.product();
+
+// Function class 4
+
+class Tarea {
+  constructor(materia, nota) {
+    this.materia = materia;
+    this.nota = nota;
   }
-  articulo() {
-    if (this.sku) {
-      console.log(
-        `Articulo: ${this.sku}. \nDescription: ${this.description}. \nPrecio: ${this.price}.`
-      );
+  thisMateria() {
+    if (this.materia && this.nota >= 6) {
+      console.log(`Aprobado! \nMateria: ${this.materia} \nNota: ${this.nota}`);
     } else {
-      console.log(`No se encontro ningun articulo: ${this.sku}.`);
+      console.log("Desaprobado.");
     }
   }
 }
 
-const producto = new Product("CA1", "Camisa rayada", 19900);
-producto.articulo();
-
-// Function class 2:
-
-class Auto {
-  constructor(marca, modelo, km) {
-    this.marca = marca;
-    this.modelo = modelo;
-    this.km = km;
-  }
-  marcaAuto() {
-    if (this.marca && this.modelo) {
-      console.log(
-        `Marca: ${this.marca}. \nModelo: ${this.modelo}. \nKM: ${this.km}.`
-      );
-    } else {
-      console.log(
-        `No se encontro la marca ${this.marca} ni modelo ${this.modelo}`
-      );
-    }
-  }
-}
-
-const autito = new Auto("Toyota", "Corolla", 60000);
-autito.marcaAuto();
+const material = new Tarea("Matematica", 6);
+material.thisMateria();
