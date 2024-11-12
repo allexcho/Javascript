ae481f1e71b030e71d348d6d7c4543a8c9bdae8a
Alex <allexcho98@gmail.com>
2024-11-11 21:38:55 -0300

    clase

--- Orden Superior/main.js
+++ Orden Superior/main.js
@@ -80,3 +80,161 @@ Higher order function | Orden superior
 //     numDuplicado.push(elemento * 2);
 // })
 // console.log(numDuplicado)
+
+/* 
+Ejemplo que simula el llamado a una API para obtener datos de usuarios, aplicando una función de orden superior para procesar cada "respuesta" de la API:
+*/
+
+// Creo una function para almacenar los usuarios
+// let usuarios = [];
+
+// function simularLlamadaAPI(array, funcion) {
+//     for (let i = 0; i < array.length; i++) {
+//     // Simulo un llamado a una API que devuelve un objeto con los datos del usuario
+//     console.log(`Llamado a la API: ${array[i]}`)
+//     funcion(array[i]);
+//     };
+// }
+
+// let nombres = ["Juan", "Pedro", "Maria", "Ana"];
+
+// simularLlamadaAPI(nombres, (nombre) => {
+//     usuarios.push({name: nombre, id: usuarios.length + 1});
+// })
+
+// console.log(usuarios);
+
+/* 
+Metodos de busqueda y transformacion:
+forEach(), find(), filter(), some(), map(), reduce(), sort() 
+*/
+
+/* Metodo forEach(): 
+
+const numeros = [1, 2, 3, 4, 5];
+numeros.forEach((numero) => {
+    console.log(numero);
+});
+
+// const arrayMixto = [1, "Hola", true, { nombre: "Juan" }, [1, 2, 3]];
+
+// arrayMixto.forEach(elemento => {
+//     if (typeof elemento === "number") {
+//         console.log(`El elemento ${elemento} es un número.`);
+//     } else if (typeof elemento === "string") {
+//         console.log(`El elemento ${elemento} es un string.`);
+//     } else if (typeof elemento === "boolean") {
+//         console.log(`El elemento ${elemento} es un boolean.`);
+//     } else if (typeof elemento === "object") {
+//         console.log(`El elemento ${elemento} es un objeto.`);
+//     }
+// })
+
+*/
+
+/* Find: Compara por parametros */
+
+// const cursos = [
+//     { id: 1, nombre: "JavaScript", price: 11600, duration: 2 },
+//     { id: 2, nombre: "Python", price: 5800, duration: 1 }
+// ]
+
+// const resultado = cursos.find((curso) => curso.nombre === "JavaScript");
+// const resultado2 = cursos.find((curso) => curso.nombre === "C++");
+
+// console.log(resultado); 
+// console.log(resultado2);
+
+/* Filter: Compara por parametro, devuelve lo que busque en un array */
+
+// const cursos = [
+//     { id: 1, nombre: "C+", price: 11600, duration: 2 },
+//     { id: 2, nombre: "C++", price: 5800, duration: 1 },
+//     { id: 3, nombre: "React", price: 11600, duration: 2 },
+//     { id: 4, nombre: "Vite", price: 5800, duration: 1 }
+// ]
+
+// const resultado = cursos.filter((curso) => {
+//     curso.nombre.includes("C++");
+// });
+
+// console.log(resultado);
+
+// const resultado2 = cursos.filter((curso) => curso.price > 10000);
+
+// console.log(resultado2);
+
+/* Some: Returns true o false (BOOLEAN) */
+
+// const cursos = [
+//     { id: 1, nombre: "C+", price: 11600, duration: 2 },
+//     { id: 2, nombre: "C++", price: 5800, duration: 1 },
+//     { id: 3, nombre: "React", price: 11600, duration: 2 },
+//     { id: 4, nombre: "Vite", price: 5800, duration: 1 }
+// ]
+
+// console.log(cursos.some((curso) => curso.id === 0)); // false
+// console.log(cursos.some((curso) => curso.id === 1)); // true
+// console.log(cursos.some(curso => curso.price > 10000)); // true
+
+/* Map: */
+
+// const cursos = [
+//     { id: 1, nombre: "C+", price: 11600, duration: "1 ano" },
+//     { id: 2, nombre: "C++", price: 5800, duration: "1.6 anos" },
+//     { id: 3, nombre: "React", price: 11600, duration: "2 anos" },
+//     { id: 4, nombre: "Vite", price: 5800, duration: "1 ano" }
+// ]
+
+// const nombres = cursos.map((curso) => curso.duration);
+// // console.log(nombres);
+// const actualizacion = cursos.map((curso) => {
+//     return {
+//         nombres: curso.nombre,
+//         priceConIVA: curso.price * 1.21
+//     }
+// })
+
+// console.log(actualizacion);
+// console.log(cursos);
+
+/* Reduce: Obtener un unico valor (Recibe 2 parametros) */
+
+// const number = [1, 2, 3, 4, 5];
+
+// const total = number.reduce((acumulador, elemento) => acumulador + elemento, 0);    // El 0 es el valor inicial del acumulador
+
+// console.log(total);
+
+const product = [
+    {id: 0, nombre: "Blusa", precio: 14900},
+    {id: 1, nombre: "Pantalón", precio: 19900},
+    {id: 2, nombre: "Remera", precio: 11900},
+    {id: 3, nombre: "Camisa", precio: 22900}
+]
+
+// FIND: Metodo para buscar un producto segun su id:
+
+const itemFound = product.find(item => item.id === 0);
+
+// SOME: Metodo para verificar si hay algun producto que cumpla con una condicion:
+
+const hasProduct = product.some(item => item.nombre === "Remeron"); // Devuelve False
+
+// FILTER: Metodo para filtrar los productos segun una condicion:
+
+const productsFiltered = product.filter(item => item.precio > 15000);
+
+// MAP: Metodo para transformar cada elemento del array:
+
+const productsNames = product.map(item => item.nombre);
+
+// REDUCE: Metodo para reducir el array a un solo valor:
+
+const totalPrice = product.reduce((total, item) => total + item.precio, 0);
+
+console.log(itemFound);
+console.log(hasProduct);
+console.log(productsFiltered)
+console.log(productsNames);
+console.log(productsFiltered);
